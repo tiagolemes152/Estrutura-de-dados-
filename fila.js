baedb0ce127ab7d968b7f0ef6d6578bc51362d7f
Tiago Lemes <tiagolemes1414@gmail.com>
2026-03-18 06:11:10 -0300

    atividade 18/03/26

---
+++ fila.js
@@ -0,0 +1,19 @@
+class fila
+{
+    #inicio;
+    #fim;
+    #qnt;
+    #elementos;
+
+    constructor(tam=10)
+    {
+        this.fim= -1;
+        this.inicio= 0;
+        this.qnt = 0;
+        this.elementos = new Array(tamanho);
+    }
+    //enqueue
+    //isfull
+    //dequeue
+    //isempety
+}
